93916dea6d0facf20938e18739fe55417cd4abc1
mohsin <mohsinbwp2018@gmail.com>
2022-08-02 19:59:12 +0500

    changed till card1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      backgroundImage: {
+        'header-background': "url('/public/images/slide1.jgp')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      },
+      fontFamily: {
+        'Monsterrate':'Montserrat',
+        'Vollkorn':'Vollkorn',
+      },
+      fontSize:
+      {
+          'heading':'1.6rem'
+      },
+      lineHeight:
+      {
+        'heading-height':'1.8rem'
+      }
+ 
+      }
+
+    },
+
   plugins: [],
-}
+  }
+

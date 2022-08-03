f2396a0eabe2c9ac979bd16ebc5d84355e8fcf34
mohsin <mohsinbwp2018@gmail.com>
2022-08-03 13:04:24 +0500

    services section creaed

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,24 @@ module.exports = {
       },
       fontSize:
       {
-          'heading':'1.6rem'
+          'heading':'1.6rem',
+          'service-heading':'1.65rem',
+          'paragraph_size':'0.8rem'
       },
       lineHeight:
       {
         'heading-height':'1.8rem'
+      },
+      colors:
+      {
+        figure_heading:
+        {
+          600:"#333333",
+        },
+        services_paragraph:
+        {
+          300:'#5c5c5c'
+        }
       }
  
       }
